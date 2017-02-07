be2b4147c1b700625e89af2071a211142b525190
tatu-polvinen <tatu.polvinen@gmail.com>
2017-02-07 20:27:48 +0200

    Testisähläys

---
+++ src/test/java/com/polvinen/bookstore/domain/BookTest.java
@@ -0,0 +1,13 @@
+package com.polvinen.bookstore.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tatu on 07/02/17.
+ */
+public class BookTest {
+    @Test
+
+}
