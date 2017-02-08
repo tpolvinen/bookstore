dbf7add43a7bebdd01f4557a425c2bc28c1bbd97
tatu-polvinen <tatu.polvinen@gmail.com>
2017-02-08 22:04:48 +0200

    Tehtävä 3.3

--- src/main/java/com/polvinen/bookstore/web/BookController.java
+++ src/main/java/com/polvinen/bookstore/web/BookController.java
@@ -4,11 +4,11 @@ package com.polvinen.bookstore.web;
  * Created by tatu on 06/02/17.
  */
 
-import com.polvinen.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 import com.polvinen.bookstore.domain.Book;
@@ -28,5 +28,23 @@ public class BookController {
     public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.delete(bookId);
+        return "redirect:../booklist";
     }
 }
