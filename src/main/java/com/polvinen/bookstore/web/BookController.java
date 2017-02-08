9ac0e6adeb2cb9ea08c7d85e09ec67aef20ff3eb
tatu-polvinen <tatu.polvinen@gmail.com>
2017-02-08 20:37:40 +0200

    Tehtävä 3.2

--- src/main/java/com/polvinen/bookstore/web/BookController.java
+++ src/main/java/com/polvinen/bookstore/web/BookController.java
@@ -4,15 +4,29 @@ package com.polvinen.bookstore.web;
  * Created by tatu on 06/02/17.
  */
 
+import com.polvinen.bookstore.domain.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import com.polvinen.bookstore.domain.Book;
+import com.polvinen.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+    @Autowired
+    private BookRepository repository;
 
-    @RequestMapping(value="/index", method=RequestMethod.GET)
-    public String indexForm() {
-        return "index";
+//    @RequestMapping(value="/index", method=RequestMethod.GET)
+//    public String indexForm() {
+//        return "index";
+//    }
+
+    @RequestMapping(value="/booklist", method=RequestMethod.GET)
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
