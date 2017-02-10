b5d5513d05c12bcde94269ef9c66a791d82bae9d
tatu-polvinen <tatu.polvinen@gmail.com>
2017-02-10 17:08:25 +0200

    Tehtävä 4.2 vieläkin KESKEN -admin ei oikeassa roolissa

---
+++ src/main/java/com/polvinen/bookstore/domain/SignupForm.java
@@ -0,0 +1,67 @@
+package com.polvinen.bookstore.domain;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class SignupForm {
+    @NotEmpty
+    @Size(min=5, max=30)
+    private String username = "";
+
+    @NotEmpty
+    @Size(min=7, max=30)
+    private String password = "";
+
+    @NotEmpty
+    @Size(min=7, max=30)
+    private String passwordCheck = "";
+
+    @NotEmpty
+	private  String email = "";
+
+    @NotEmpty
+    private String role = "";
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordCheck() {
+		return passwordCheck;
+	}
+
+	public void setPasswordCheck(String passwordCheck) {
+		this.passwordCheck = passwordCheck;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    
+}
